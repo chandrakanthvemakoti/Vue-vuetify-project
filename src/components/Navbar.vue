395edfc37b00f3721771de6e36587f14b51498a3
chandrakanthvemakoti <chanduvemakoti@gmail.com>
2020-04-21 18:05:43 +0530

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+<nav>
+  <v-toolbar  dark flat app color="indigo">
+    <v-toolbar-title>
+        <span>VUE JS PROJECT</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn flat color="indigo">
+        <span>Sign In</span>
+    </v-btn>
+    <v-btn flat color="indigo">
+        <span>Sign Up</span>
+    </v-btn>
+    <v-btn flat color="indigo">
+        <span>Dashboard</span>
+    </v-btn>
+  </v-toolbar>
+  <v-navigation-drawer app v-model="drawer" dark> 
+  hello
+  </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer:false
+        }
+    }
+}
+</script>
